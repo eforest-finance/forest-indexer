56ad0983611657fee821c33031f8077426422fa6
kevin <kevin@eforest.finance>
2024-10-17 20:14:07 +0800

    add AuctionTimeUpdatedLogEventProcessor

--- src/Forest.Indexer.Plugin/Processors/AuctionTimeUpdatedLogEventProcessor.cs
+++ src/Forest.Indexer.Plugin/Processors/AuctionTimeUpdatedLogEventProcessor.cs
@@ -2,6 +2,7 @@ using AeFinder.Sdk.Logging;
 using AeFinder.Sdk.Processor;
 using Forest.Contracts.Auction;
 using Forest.Indexer.Plugin.Entities;
+using Forest.Indexer.Plugin.enums;
 using Forest.Indexer.Plugin.Util;
 using Volo.Abp.ObjectMapping;
 
@@ -10,11 +11,15 @@ namespace Forest.Indexer.Plugin.Processors;
 public class AuctionTimeUpdatedLogEventProcessor : LogEventProcessorBase<AuctionTimeUpdated>
 {
     private readonly IObjectMapper _objectMapper;
-    
+    private readonly IAElfClientServiceProvider _aElfClientServiceProvider;
+
     public AuctionTimeUpdatedLogEventProcessor(
-        IObjectMapper objectMapper)
+        IObjectMapper objectMapper,
+        IAElfClientServiceProvider aElfClientServiceProvider
+    )
     {
         _objectMapper = objectMapper;
+        _aElfClientServiceProvider = aElfClientServiceProvider;
     }
 
 
@@ -81,7 +86,7 @@ public class AuctionTimeUpdatedLogEventProcessor : LogEventProcessorBase<Auction
     }
     private async Task<TsmSeedSymbolIndex> GetTsmSeedAsync(string chainId, string seedSymbol)
     {
-        var mustQuery = new List<Func<QueryContainerDescriptor<TsmSeedSymbolIndex>, QueryContainer>>
+        /*var mustQuery = new List<Func<QueryContainerDescriptor<TsmSeedSymbolIndex>, QueryContainer>>
         {
             q => q.Term(i => i.Field(f => f.ChainId)
                 .Value(chainId)),
@@ -94,6 +99,25 @@ public class AuctionTimeUpdatedLogEventProcessor : LogEventProcessorBase<Auction
             f.Bool(b => b.Must(mustQuery));
 
         var result = await _tsmSeedSymbolIndexRepository.GetListAsync(Filter);
-        return result.Item2.IsNullOrEmpty() ? null : result.Item2.FirstOrDefault();
+        return result.Item2.IsNullOrEmpty() ? null : result.Item2.FirstOrDefault();*/
+        //todo V2 getTsmSeedInfo from Contract.need test by self
+        var tokenContractAddress = ContractInfoHelper.GetTokenContractAddress(chainId);
+        var tokenInfo =
+            await _aElfClientServiceProvider.GetTokenInfoAsync(chainId, tokenContractAddress, seedSymbol);
+        if (tokenInfo == null)
+        {
+            return null;
+        }
+        var seedOwnedSymbol = EnumDescriptionHelper.GetExtraInfoValue(tokenInfo.ExternalInfo, TokenCreatedExternalInfoEnum.SeedOwnedSymbol);
+        if (seedOwnedSymbol == null)
+        {
+            return null;
+        }
+
+        var seedSymbolIndexId = IdGenerateHelper.GetTsmSeedSymbolId(chainId, seedOwnedSymbol);
+        return new TsmSeedSymbolIndex()
+        {
+            Id = seedSymbolIndexId
+        };
     }
 }
