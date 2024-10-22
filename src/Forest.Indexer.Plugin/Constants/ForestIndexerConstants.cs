8fab886bae431592e433d7aa6eb57a8a1a551977
kevin <kevin@eforest.finance>
2024-10-22 17:00:27 +0800

    add contract config

--- src/Forest.Indexer.Plugin/Constants/ForestIndexerConstants.cs
+++ src/Forest.Indexer.Plugin/Constants/ForestIndexerConstants.cs
@@ -56,7 +56,8 @@ public static partial class ForestIndexerConstants
     public const int IntZero = 0;
     public const int IntOne = 0;
     public const int IntTen = 10;
-    public const string SGRCollection = "SGR-0";//todo V2 test config is not SGR-0,need modify
+    public const string SGRCollection = "SGR-0";
+
     public const int SGRDecimal = 8;
     public const int QueryUserBalanceListDefaultSize = 3000;
     
@@ -64,33 +65,44 @@ public static partial class ForestIndexerConstants
     public const string TDVV = "tDVV";
     public const string TDVW = "tDVW";
 
-    public const string NFTForestContractAddressAELF = "";
-    public const string NFTForestContractAddressTDVV = "";
-    public const string NFTForestContractAddressTDVW = "";
+    public const string NFTForestContractAddressAELF = "";//not have
+    public const string NFTForestContractAddressTDVV = "2cGT3RZZy6UJJ3eJPZdWMmuoH2TZBihvMtAtKvLJUaBnvskK2x";
+    public const string NFTForestContractAddressTDVW = "zv7YnQ2dLM45ssfifN1dpwqBwdxH13pqGm9GDH6peRdH8F3hD";
+    //test
+    public const string TokenAdaptorContractAddressAELF = "gjGmHom31GWr5VPWf11de3mJGHVdaDFsR4zgrqjrbijYXv6TW";
+    //prod todo V2
+    //public const string TokenAdaptorContractAddressAELF = "ZYNkxNAzswRC8UeHc6bYMdRmbmLqYDPqZv7sE5d9WuJ5rRQEi";
+
+    public const string TokenAdaptorContractAddressTDVV = "";//not have
+    public const string TokenAdaptorContractAddressTDVW = "";//not have
     
-    public const string TokenAdaptorContractAddressAELF = "";
-    public const string TokenAdaptorContractAddressTDVV = "";
-    public const string TokenAdaptorContractAddressTDVW = "";
-    
-    public const string TokenContractAddressAELF = "";
-    public const string TokenContractAddressTDVV = "";
-    public const string TokenContractAddressTDVW = "";
+    //test
+    public const string TokenContractAddressAELF = "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE";
+    //prod
+   // public const string TokenContractAddressAELF = "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE";
+    public const string TokenContractAddressTDVV = "7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX";
+    public const string TokenContractAddressTDVW = "ASh2Wt7nSEmYqnGxPPzp4pnVDU4uhj1XW9Se5VeZcX2UDdyjx";
     
     public const string AuctionContractAddressAELF = "";
     public const string AuctionContractAddressTDVV = "";
-    public const string AuctionContractAddressTDVW = "";
-    
-    public const string SymbolRegistrarContractAddressAELF = "";
+    public const string AuctionContractAddressTDVW = "1EFmvua5WQiv15N3xF4egEUvkvLGNWHdoYLMcbXdaXxzrGmA";
+    //test
+    public const string SymbolRegistrarContractAddressAELF = "SRVEHfZoiifcHYfnTagJvtW3QtkGnVo1rEEssKk8hirHX8xed";\
+    //prod
+    //public const string SymbolRegistrarContractAddressAELF = "";
+
     public const string SymbolRegistrarContractAddressTDVV = "";
     public const string SymbolRegistrarContractAddressTDVW = "";
+    //test
+    public const string ProxyAccountContractAddressAELF = "2QbymcZLDgQErCixb3r87tLChDhpAueDK6tit9et9B5kvZfrqG";
+    //prod
+    //public const string ProxyAccountContractAddressAELF = "2UM9eusxdRyCztbmMZadGXzwgwKfFdk8pF4ckw58D769ehaPSR";
+    public const string ProxyAccountContractAddressTDVV = "hg7hFigUZ6W3gLreo1bGnpAQTQpGsidueYBScVpzPAi81A2AA";
+    public const string ProxyAccountContractAddressTDVW = "C6fn7Cb1QJbgw8FjYHQSoUBbnsii6uKqD5eneQUsDPWpr62kJ";
     
-    public const string ProxyAccountContractAddressAELF = "";
-    public const string ProxyAccountContractAddressTDVV = "";
-    public const string ProxyAccountContractAddressTDVW = "";
-    
-    public const string GenesisContractAddressAELF = "";
-    public const string GenesisContractAddressTDVV = "";
-    public const string GenesisContractAddressTDVW = "";
+    public const string GenesisContractAddressAELF = "pykr77ft9UUKJZLVq15wCH8PinBSjVRQ12sD1Ayq92mKFsJ1i";
+    public const string GenesisContractAddressTDVV = "2dtnkWDyJJXeDRcREhKSZHrYdDGMbn3eus5KYpXonfoTygFHZm";
+    public const string GenesisContractAddressTDVW = "2dtnkWDyJJXeDRcREhKSZHrYdDGMbn3eus5KYpXonfoTygFHZm";
     public const int MaxWriteDBRecord = 80;
     public const int MaxQueryCount = 5;
     public const int MaxQuerySize = 10000;
