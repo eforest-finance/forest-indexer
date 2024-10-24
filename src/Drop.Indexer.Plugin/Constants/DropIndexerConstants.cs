53b0177d039503c1d8e0a2d74d1e3c8a71748184
theodore <theodore@eforest.finance>
2024-10-24 13:46:57 +0800

    feat: modify DropIndexerConstants

--- src/Drop.Indexer.Plugin/Constants/DropIndexerConstants.cs
+++ src/Drop.Indexer.Plugin/Constants/DropIndexerConstants.cs
@@ -2,14 +2,11 @@ namespace Drop.Indexer.Plugin;
 
 public static class DropIndexerConstants
 {
-    public const string QueryDropListScript =
-        "Date date = new Date();long currentTime = date.getTime();long startTime = doc['startTime'].value.toInstant().toEpochMilli();long expireTime = doc['expireTime'].value.toInstant().toEpochMilli();if(startTime < currentTime && expireTime > currentTime) return startTime; else if(startTime > currentTime) return startTime*2; else return startTime*3;";
-
     public const string AELF = "AELF";
     public const string TDVV = "tDVV";
     public const string TDVW = "tDVW";
 
     public const string NFTDropContractAddressAELF = "";
-    public const string NFTDropContractAddressTDVV = "";
+    public const string NFTDropContractAddressTDVV = "XmQ59e3JxmtP5gGafNFyJQAF5A2WbtVDYXFVv3JEaKMckyb3b";
     public const string NFTDropContractAddressTDVW = "yEMwBeheRq6iiw6VN9TgUt2eASBNcgxsEUUwFFsgXedySgnp2";
 }
