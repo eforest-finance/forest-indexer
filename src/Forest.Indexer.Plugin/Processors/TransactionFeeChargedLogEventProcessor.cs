d5a266b0cac92301e6e3310dbf1fff476b7d4164
theodore <theodore@eforest.finance>
2024-11-04 17:36:21 +0800

    feat: avoid TransactionFeeChargedLogEventProcessor

--- src/Forest.Indexer.Plugin/Processors/TransactionFeeChargedLogEventProcessor.cs
+++ src/Forest.Indexer.Plugin/Processors/TransactionFeeChargedLogEventProcessor.cs
@@ -30,6 +30,7 @@ public class TransactionFeeChargedLogEventProcessor : LogEventProcessorBase<Tran
 
     public async override Task ProcessAsync(TransactionFeeCharged eventValue, LogEventContext context)
     {
+        return;//todo v2
         // Logger.LogDebug("TransactionFeeChargedLogEventProcessor-1 {A}",JsonConvert.SerializeObject(eventValue));
         // Logger.LogDebug("TransactionFeeChargedLogEventProcessor-2 {A}",JsonConvert.SerializeObject(context));
         if (eventValue == null) return;
